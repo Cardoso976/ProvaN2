7fafd5b4a20f85f3663ab39cd6560c6cc4efab0a
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-10-27 14:26:27 -0200

    Rota default Cadastro/Index

--- ProvaN2.Web/ProvaN2/Scripts/Cadastro/CadastroPessoa.js
+++ ProvaN2.Web/ProvaN2/Scripts/Cadastro/CadastroPessoa.js
@@ -1,11 +1,15 @@
-﻿$("#btn_incluir").click(function () {
+﻿function abrir_form() {
     var modal_cadastro = $("#modal_cadastro");
-    
+
     bootbox.dialog({
         title: 'Cadastro de Pessoas',
         message: modal_cadastro
     })
-        .on('shown.bs.modal', function(){
+        .on('shown.bs.modal', function () {
             modal_cadastro.show();
-      });
+        });
+}
+
+$("#btn_incluir").click(function () {
+    abrir_form();
 });
